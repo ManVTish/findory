7a0e5a16a5bdb7ac268d8e844e6874f39603e112
Manish <manishroy0112@gmail.com>
2022-12-24 18:41:58 +0530

    commente added

--- config/project_variables.py
+++ config/project_variables.py
@@ -1,6 +1,9 @@
 from decouple import config
 
+# APP domain
 APP_HOST = config('HOST')
+
+# Redis DB
 REDIS_STORAGE_HOST = config('DB_STORAGE_HOST')
 REDIS_PORT = config('DB_PORT')
 REDIS_STORAGE_PASSWORD = config('DB_STORAGE_PASSWORD')
