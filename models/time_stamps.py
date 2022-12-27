7419f0188e09cf574f4266fccfc910c78920bbd7
Manish <manishroy0112@gmail.com>
2022-12-27 21:38:56 +0530

    notes field type updated

--- models/time_stamps.py
+++ models/time_stamps.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
-from typing import Optional
+
+from models.notes import Notes, Optional, BaseModel
 
 class TimeStamps(BaseModel):
     video_pk_id: str
     user_id: str
-    notes: str # type Notes
+    notes: Notes
     start_time: str
     end_time: str
 
