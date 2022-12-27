4ad06f8b9bc01f29c5314fc836649101fe4a5eaa
Manish <manishroy0112@gmail.com>
2022-12-27 22:07:18 +0530

    video model updated for new design

--- models/video.py
+++ models/video.py
@@ -1,13 +1,11 @@
 # from config.database import redis
 
-from redis_om import JsonModel
 from models.common_lib import Optional, BaseModel, List
 
 class Video(BaseModel):
-    user_video_id: str
-    user_video_transcript: Union[List, None] = None
-
-
-    class Meta:
-        database = redis
+    source_video_id: str
+    user_id: str
+    keyword: str
+    transcripts: Optional[List] = None
+    time_stamp: TimeStamps
 
