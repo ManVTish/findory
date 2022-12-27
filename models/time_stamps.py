aa9171d190cdb6d3b5a56f4693e4265989401c05
Manish <manishroy0112@gmail.com>
2022-12-27 21:56:35 +0530

    common lib file added

--- models/time_stamps.py
+++ models/time_stamps.py
@@ -1,5 +1,5 @@
-
-from models.notes import Notes, Optional, BaseModel
+from models.common_lib import Optional, BaseModel
+from models.notes import Notes
 
 class TimeStamps(BaseModel):
     video_pk_id: str
